d5e4154c847b36153d5024c647020d6718a54209
Martyna Knell <martyna.salome@gmail.com>
2023-08-24 21:16:43 -0400

    Set up next-auth.

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+// Create a global variable to share the PrismaClient instance across your app
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
+// Create a constant to check whether PrismaClient is already initialized
 const client = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
